33354b6440fac7223d765f007598f92ed284b56e
dingmb <1466701844@qq.com>
2023-04-15 14:51:08 +0800

    Initial commit

---
+++ src/main/java/com/dingmb/controller/HelloController.java
@@ -0,0 +1,16 @@
+package com.dingmb.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author dingmb
+ * @create 2023-04-15
+ */
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public String hello() {
+        return "git hello";
+    }
+}
